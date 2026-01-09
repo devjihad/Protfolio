fdbf5286343238ee64e65534c8eed5d8c7e1df71
devjihad <mdhajihad609@gmail.com>
2026-01-09 21:29:50 +0600

    done

--- src/Container/Container.jsx
+++ src/Container/Container.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Container = ({children}) => {
     return (
-        <div className='md:w-[800px] lg:w-300 mx-auto'>
+        <div className='w-87.5 md:w-187.5 lg:w-250 xl:w-275 2xl:w-350 mx-auto'>
             {children}
         </div>
     );
