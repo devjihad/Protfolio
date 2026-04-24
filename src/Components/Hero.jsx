290ad393113db50ea5911de7298b33b4f6096be2
devjihad <mdhajihad609@gmail.com>
2026-04-24 23:36:15 +0600

    updating

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
             
             <div className='w-full md:w-2/3 lg:w-1/2 mt-5'>
                 <h2 className='text-[28px] md:text-[40px] lg:text-[45px] font-semibold leading-10 md:leading-16 mb-5 '><span className='bg-linear-to-r/decreasing from-zinc-500 to-blue-300 bg-clip-text text-transparent'>I'm Jihad,</span> frontend developer based in USA</h2>
-                <p className='font-medium'>I'm a frontend developer creating responsive, user-friendly interfaces using modern web technologies for smooth, engaging user experiences.</p>
+                <p className='font-medium'>FrontendDeveloper with 1.5+ years of experience building responsive and high-performance web applications. Skilled in converting UI/UX designs (Figma/PSD) into functional websites and optimizing performance.</p>
                 <div className='mt-8'>
 
                     <AnchorLink offset={50} href='#contact'><button className='bg-linear-to-r/decreasing from-zinc-500 to-blue-300   px-5 py-2 rounded-lg font-medium transition duration-300 hover:scale-110 mr-6 cursor-pointer'>Connect with me</button></AnchorLink>
